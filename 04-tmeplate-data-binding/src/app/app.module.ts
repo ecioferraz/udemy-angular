b61fcf6bd1f0b26dda8939d39504d1e5b91e871b
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-17 18:54:52 -0300

    feat: session 4

---
+++ 04-tmeplate-data-binding/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TitleComponent } from './components/title/title.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+
+@NgModule({
+  declarations: [AppComponent, TitleComponent, DataBindingComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
