d1941e1bfd424d5d78ada625b1ac14f2667ab753
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-31 20:56:40 -0300

    feat: session-12

---
+++ 12-roteamento/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  { component: AboutComponent, path: 'about' },
+  { component: HomeComponent, path: '', pathMatch: 'full' },
+  { component: NotFoundComponent, path: 'not-found' },
+  { path: '**', redirectTo: 'not-found' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ 12-roteamento/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MenuComponent } from './shared/menu/menu.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, AboutComponent, NotFoundComponent, MenuComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
